f5dd64c4bb2c676312fa2dbc77fcf421a7778d7f
wangkailang <wangkailang@vikadata.com>
2026-05-19 17:22:28 +0800

    fix: 切换 HashRouter 修复 GitHub Pages 白屏
    - BrowserRouter 依赖服务端路由，GitHub Pages 纯静态托管不支持
    - HashRouter 使用 URL hash (#) 路由，完全兼容静态托管

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
 
+// HashRouter 适配 GitHub Pages 静态托管，无需服务端路由支持
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
